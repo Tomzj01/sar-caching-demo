3197eef39a18070ece9855129f505aaaf2bd0e16
Tom <zatom@deloitte.com>
2021-10-01 15:02:45 -0400

    initial commit

---
+++ src/main/java/com/zatom/sarcachingdemo/SarCachingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zatom.sarcachingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarCachingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarCachingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zatom/sarcachingdemo/SarCachingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zatom.sarcachingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarCachingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
